9f00c62c5d6aff069649450c6467a3221b635431
Henry Sotelo <henry.sotelo@silverconsulting.com.co>
2024-05-25 19:23:56 +0000

    V5 - Bloque IV Navegacion, Controles y Adaptaciones (enrutamiento y Navegación)

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -1,19 +1,48 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * 
      * @param { typeof sap.ui.core.mvc.Controller } Controller 
+     * @param { typeof sap.ui.core.routing.History } History 
      */
 
-    function (Controller) {
+    function (Controller, History) {
         'use strict';
 
         return Controller.extend("dev.invoices.controller.Details", {
 
-            onInit: function () {
+            _onObjectMatchy: function (oEvent) {
+                debugger;
 
+                this.getView().bindElement({
+                    path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+                    model: "northwind"
+                });
+
+            },
+
+            onInit: function () {
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("Details").attachPatternMatched(this._onObjectMatchy, this);
+
+            },
+
+            onNavBack: function () {
+
+                const oHistory = History.getInstance();
+                const sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1)
+                } else {
+                    const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("RouteApp", {}, true);
+                };
             }
+
+
 
         });
     });
